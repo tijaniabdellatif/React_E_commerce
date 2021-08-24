f4e9b0d43e70f646b5b90b31e18059dc72609531
tijani abdellatif <tijani.idrissi.abdellatif@gmail.com>
2021-08-24 20:54:38 +0200

    Singleproduct_components

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -52,10 +52,39 @@ const SingleProductPage = () => {
   }
 
  console.log(product);
- const {name,price,description,stock,stars,reviews,id:slug,company,images} = product;
+ const {name,price,description,stock,stars,reviews,id:slug,company,images} = product || {};
   return (
         <Wrapper>
-          <PageHero title={name} />
+          <PageHero title={name} product />
+          <div className="section section-center page">
+            <Link to="/products" className="btn">
+               Back to products
+            </Link>
+            <div className="product-center">
+              <ProductImages />
+              <section className="content">
+                <h2>{name}</h2>
+                <Stars />
+                <h5 className="price">{formatPrice(price)}</h5>
+                <p className="desc">{description}</p>
+                <p className="info">
+                  <span>Available : </span>
+                  {stock > 0 ? 'In stock' : 'put of stock'}
+                </p>
+                <p className="info">
+                  <span>Slug : </span>
+                  {slug}
+                </p>
+                <p className="info">
+                  <span>Brand : </span>
+                  {company}
+                </p>
+                <hr />
+
+                {stock > 0 && <AddToCart />}
+              </section>
+            </div>
+          </div>
         </Wrapper>
   );
 }
