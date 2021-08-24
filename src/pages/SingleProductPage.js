4804b644118f58b59e72e8f36146df984e9bffc5
tijani abdellatif <tijani.idrissi.abdellatif@gmail.com>
2021-08-25 00:46:10 +0200

    single_product_colors

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -61,10 +61,10 @@ const SingleProductPage = () => {
                Back to products
             </Link>
             <div className="product-center">
-              <ProductImages />
+              <ProductImages images={images} />
               <section className="content">
                 <h2>{name}</h2>
-                <Stars />
+                <Stars stars={stars} reviews={reviews} />
                 <h5 className="price">{formatPrice(price)}</h5>
                 <p className="desc">{description}</p>
                 <p className="info">
@@ -81,7 +81,7 @@ const SingleProductPage = () => {
                 </p>
                 <hr />
 
-                {stock > 0 && <AddToCart />}
+                {stock > 0 && <AddToCart product={product} />}
               </section>
             </div>
           </div>
